e6bf24539c0147fbbc0f4f57af59b6dd2450cf8e
Derek Leblanc <derekleblanc@me.com>
2022-08-18 00:04:48 -0300

    Fix mobile styling

--- src/Header.tsx
+++ src/Header.tsx
@@ -4,10 +4,8 @@ import "@fontsource/oswald"
 import "@fontsource/allura"
 import "@fontsource/bonheur-royale"
 
-const HEIGHT = '30px';
 const HeaderBox = chakra(Box, {
     baseStyle:{
-        height: HEIGHT,
         fontFamily: `Ruluko, sans-serif`,
         fontSize: '16px'
     }
@@ -15,8 +13,8 @@ const HeaderBox = chakra(Box, {
 
 export const Header = () => {
     return (
-        <Box p={4}>
-            <Box textAlign='center' fontFamily={`'Sacramento', sans-serif`} fontSize={82} bgGradient='radial(custom.8, custom.8, custom.8, custom.8, custom.3, custom.3, custom.3)' color='custom.4'>Charlotte & Dylan</Box>
+        <Box >
+            <Box p={50} textAlign='center' fontFamily={`'Sacramento', sans-serif`} fontSize={{ base: '24px', md: '40px', lg: '56px' }} bgGradient='radial(custom.8, custom.8, custom.8, custom.8, custom.3, custom.3, custom.3)' color='custom.4'>Charlotte & Dylan</Box>
             <SimpleGrid columns={{base: 2, sm: 2, md: 2, lg:4, xl: 4}} textAlign='center' color='custom.4' pt={4}>
                 <HeaderBox><Link>Invitation</Link></HeaderBox>
                 <HeaderBox><Link>Schedule</Link></HeaderBox>
