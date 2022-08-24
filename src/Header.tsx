4c140215656cd5c27d553253d2f3f6bdf7e22c3a
labronwyn <noreply@labronwyn.com>
2022-08-23 23:33:02 -0300

    spurious text

--- src/Header.tsx
+++ src/Header.tsx
@@ -16,8 +16,8 @@ export const Header = () => {
         <Box >
             <Box p={50} textAlign='center' fontFamily={`'Sacramento', sans-serif`} fontSize={{ base: '24px', md: '40px', lg: '56px' }} bgGradient='radial(custom.8, custom.8, custom.8, custom.8, custom.3, custom.3, custom.3)' color='custom.4'>Charlotte & Dylan</Box>
             <SimpleGrid columns={{base: 2, sm: 2, md: 2, lg:4, xl: 4}} textAlign='center' color='custom.4' pt={4}>
-                <HeaderBox><Link>Invitation</Link></HeaderBox>x
-                <HeaderBox><Link>Schedule</Link></HeaderBox>xw
+                <HeaderBox><Link>Invitation</Link></HeaderBox>
+                <HeaderBox><Link>Schedule</Link></HeaderBox>
                 <HeaderBox><Link>RSVP</Link></HeaderBox>
                 <HeaderBox><Link>Accommodations</Link></HeaderBox>
             </SimpleGrid>
