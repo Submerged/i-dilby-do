8943bc8d3d0e7892606122405df0870dd342a21e
labronwyn <noreply@labronwyn.com>
2022-08-23 23:32:23 -0300

    fix merge issues

--- src/Header.tsx
+++ src/Header.tsx
@@ -13,16 +13,11 @@ const HeaderBox = chakra(Box, {
 
 export const Header = () => {
     return (
-<<<<<<< HEAD
-        <Box p={4}>
-            <Box textAlign='center' fontFamily={`'Sacramento', sans-serif`} fontSize={84} color='custom.4'>Charlotte & Dylan</Box>
-=======
         <Box >
             <Box p={50} textAlign='center' fontFamily={`'Sacramento', sans-serif`} fontSize={{ base: '24px', md: '40px', lg: '56px' }} bgGradient='radial(custom.8, custom.8, custom.8, custom.8, custom.3, custom.3, custom.3)' color='custom.4'>Charlotte & Dylan</Box>
->>>>>>> 26e75d4e47a93f73d768f1cb240bdb6a36415e31
             <SimpleGrid columns={{base: 2, sm: 2, md: 2, lg:4, xl: 4}} textAlign='center' color='custom.4' pt={4}>
-                <HeaderBox><Link>Invitation</Link></HeaderBox>
-                <HeaderBox><Link>Schedule</Link></HeaderBox>
+                <HeaderBox><Link>Invitation</Link></HeaderBox>x
+                <HeaderBox><Link>Schedule</Link></HeaderBox>xw
                 <HeaderBox><Link>RSVP</Link></HeaderBox>
                 <HeaderBox><Link>Accommodations</Link></HeaderBox>
             </SimpleGrid>
