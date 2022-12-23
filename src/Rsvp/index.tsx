bc891f7836bf0146e27d02ddacbc2019142fe377
labronwyn <noreply@labronwyn.com>
2022-12-23 15:32:48 -0330

    UI tweaks

--- src/Rsvp/index.tsx
+++ src/Rsvp/index.tsx
@@ -21,7 +21,7 @@ import {
   RadioGroup,
   Radio,
   Stack,
-  HStack,
+  Box,
   AlertIcon,
   Spinner,
 } from "@chakra-ui/react";
@@ -80,125 +80,127 @@ export const Rsvp = () => {
 
   // @ts-ignore
   return (
-    <Container id='rsvp' p={[10,10,100]} maxWidth={1000}>
-      <Center paddingBottom={10}><Text as='h2'>RSVP</Text></Center>
+    <Container id='rsvp' p={[10,10,100]} maxWidth={1450}>
       <Center>
-        <form onSubmit={handleSubmit(doSubmit)}>
-          <SimpleGrid columns={[1,1,3]} spacing={5}>
-          {fields.map((item, index) => (
-            <>
-              <FormControl key={`guests.${index}.name`} isInvalid={Boolean(errors.guests)}>
-                <FormLabel htmlFor='name'>Full Name</FormLabel>
+      <Box width='100vw' border='30px solid #FFF' padding={[50,50,50]} backgroundImage="url('/i-dilby-do/euc-bg.png')" backgroundSize={'cover'} backgroundPosition="center" backgroundRepeat="no-repeat">
+        <Center paddingBottom={10}><Text as='h2'>RSVP</Text></Center>
+          <form onSubmit={handleSubmit(doSubmit)}>
+            <SimpleGrid columns={[1,1,3]} spacing={5}>
+            {fields.map((item, index) => (
+              <>
+                <FormControl key={`guests.${index}.name`} isInvalid={Boolean(errors.guests)}>
+                  <FormLabel htmlFor='name'>Full Name</FormLabel>
+                  <Input
+                    id='name'
+                    placeholder='Enter Full Name'
+                    {...register(`guests.${index}.name`, {
+                      required: 'Full Name Required',
+                      minLength: { value: 3, message: 'Minimum length should be 3' },
+                    })}
+                  />
+                  <ErrorMessage
+                    errors={errors}
+                    name={`guests.${index}.name`}
+                    render={({ message }) => <FormErrorMessage style={{fontWeight: 'bold'}}>{message}</FormErrorMessage>}/>
+                </FormControl>
+                <FormControl key={`guests.${index}.food`} isInvalid={Boolean(errors.guests)}>
+                  <FormLabel>Food Choice</FormLabel>
+                  <Select backgroundColor={'white'} errorBorderColor={'none'} {...register(`guests.${index}.food`, {
+                    required: 'Food Selection Required',
+                    minLength: { value: 4, message: 'Minimum length should be 4' },
+                  })}>
+                    <option>Beef</option>
+                    <option>Chicken</option>
+                    <option>Vegetarian</option>
+                  </Select>
+                  <ErrorMessage
+                    errors={errors}
+                    name={`guests.${index}.food`}
+                    render={({ message }) => <FormErrorMessage style={{fontWeight: 'bold'}}>{message}</FormErrorMessage>}/>
+                </FormControl>
+                <FormControl key={`guests.${index}.restrictions`}>
+                  <FormLabel htmlFor='restrictions'>Food Restrictions</FormLabel>
+                  <InputGroup>
+                    <Input
+                      id='test'
+                      placeholder='Enter Food Restrictions'
+                      {...register(`guests.${index}.restrictions`)}
+                    />
+                    <InputRightAddon>
+                      <IconButton icon={<CloseIcon />} colorScheme='blackAlpha' onClick={() => remove(index)}  aria-label={'remove button'}/>
+                    </InputRightAddon>
+                  </InputGroup>
+                </FormControl>
+              </>
+            ))}
+            </SimpleGrid>
+            <Center p={10}>
+              <Button m={4} rightIcon={<HamburgerIcon />} colorScheme='teal' onClick={() => append({ name: '', food: '', restrictions: ''})}>
+                Add a Guest
+              </Button>
+            </Center>
+            <SimpleGrid columns={[1,1,3]} spacing={4}>
+              <FormControl key={`email`} isInvalid={Boolean(errors.email)}>
+                <FormLabel htmlFor='email'>Email</FormLabel>
                 <Input
-                  id='name'
-                  placeholder='Enter Full Name'
-                  {...register(`guests.${index}.name`, {
-                    required: 'Full Name Required',
-                    minLength: { value: 3, message: 'Minimum length should be 3' },
+                  id='email'
+                  placeholder='Enter Email Address'
+                  {...register(`email`, {
+                    required: 'Email Required',
+                    minLength: { value: 4, message: 'Minimum length should be 4' },
                   })}
                 />
                 <ErrorMessage
                   errors={errors}
-                  name={`guests.${index}.name`}
+                  name={`email`}
                   render={({ message }) => <FormErrorMessage style={{fontWeight: 'bold'}}>{message}</FormErrorMessage>}/>
               </FormControl>
-              <FormControl key={`guests.${index}.food`} isInvalid={Boolean(errors.guests)}>
-                <FormLabel>Food Choice</FormLabel>
-                <Select errorBorderColor={'none'} {...register(`guests.${index}.food`, {
-                  required: 'Food Selection Required',
-                  minLength: { value: 4, message: 'Minimum length should be 4' },
-                })}>
-                  <option>Beef</option>
-                  <option>Chicken</option>
-                  <option>Vegetarian</option>
-                </Select>
-                <ErrorMessage
-                  errors={errors}
-                  name={`guests.${index}.food`}
-                  render={({ message }) => <FormErrorMessage style={{fontWeight: 'bold'}}>{message}</FormErrorMessage>}/>
+              <FormControl>
+                <FormLabel># of Children</FormLabel>
+                <NumberInput min={0} max={12}>
+                  <NumberInputField {...register('children')} />
+                  <NumberInputStepper>
+                    <NumberIncrementStepper />
+                    <NumberDecrementStepper />
+                  </NumberInputStepper>
+                </NumberInput>
               </FormControl>
-              <FormControl key={`guests.${index}.restrictions`}>
-                <FormLabel htmlFor='restrictions'>Food Restrictions</FormLabel>
-                <InputGroup>
-                  <Input
-                    id='test'
-                    placeholder='Enter Food Restrictions'
-                    {...register(`guests.${index}.restrictions`)}
-                  />
-                  <InputRightAddon>
-                    <IconButton icon={<CloseIcon />} colorScheme='teal' onClick={() => remove(index)}  aria-label={'remove button'}/>
-                  </InputRightAddon>
-                </InputGroup>
+              <Controller
+                name="attendance"
+                control={control}
+                render={({ field: { onChange, value } }) => (
+                  <RadioGroup onChange={onChange} value={value} style={{fontWeight: 'bold', borderColor: 'black'}}>
+                    <Stack direction="row">
+                      <Radio defaultChecked colorScheme='teal' value={'Accept'} checked>Accept With Pleasure</Radio>
+                      <Radio colorScheme='orange' value={'Decline'}>Decline With Regret</Radio>
+                    </Stack>
+                  </RadioGroup>
+                )}
+              />
+            </SimpleGrid>
+            <Center mt={10}>
+              <FormControl key={`music`}>
+                <FormLabel htmlFor='music'>Music Suggestion</FormLabel>
+                <Input
+                  id='music'
+                  placeholder='Do you have a music suggestion? Enter it!'
+                  {...register(`music`)}
+                />
               </FormControl>
-            </>
-          ))}
-          </SimpleGrid>
-          <Center p={10}>
-            <Button m={4} rightIcon={<HamburgerIcon />} colorScheme='teal' onClick={() => append({ name: '', food: '', restrictions: ''})}>
-              Add a Guest
-            </Button>
-          </Center>
-          <SimpleGrid columns={[1,1,3]} spacing={4}>
-            <FormControl key={`email`} isInvalid={Boolean(errors.email)}>
-              <FormLabel htmlFor='email'>Email</FormLabel>
-              <Input
-                id='email'
-                placeholder='Enter Email Address'
-                {...register(`email`, {
-                  required: 'Email Required',
-                  minLength: { value: 4, message: 'Minimum length should be 4' },
-                })}
-              />
-              <ErrorMessage
-                errors={errors}
-                name={`email`}
-                render={({ message }) => <FormErrorMessage style={{fontWeight: 'bold'}}>{message}</FormErrorMessage>}/>
-            </FormControl>
-            <FormControl>
-              <FormLabel># of Children</FormLabel>
-              <NumberInput min={0} max={12}>
-                <NumberInputField {...register('children')} />
-                <NumberInputStepper>
-                  <NumberIncrementStepper />
-                  <NumberDecrementStepper />
-                </NumberInputStepper>
-              </NumberInput>
-            </FormControl>
-            <Controller
-              name="attendance"
-              control={control}
-              render={({ field: { onChange, value } }) => (
-                <RadioGroup onChange={onChange} value={value} style={{fontWeight: 'bold', borderColor: 'black'}}>
-                  <Stack direction="row">
-                    <Radio defaultChecked colorScheme='teal' value={'Accept'} checked>Accept With Pleasure</Radio>
-                    <Radio colorScheme='teal' value={'Decline'}>Decline With Regret</Radio>
-                  </Stack>
-                </RadioGroup>
-              )}
-            />
-          </SimpleGrid>
-          <Center mt={10}>
-            <FormControl key={`music`}>
-              <FormLabel htmlFor='music'>Music Suggestion</FormLabel>
-              <Input
-                id='music'
-                placeholder='Do you have a music suggestion? Enter it!'
-                {...register(`music`)}
-              />
-            </FormControl>
-          </Center>
-          {apiState === API_SUCCESS ?
-            <Alert status='success' variant='left-accent' mt={5}>
-              <AlertIcon />
-              RSVP Received. Thank you!
-            </Alert>
-            : null}
-          <Center mt={20}>
-            <Button m={4} rightIcon={apiState === API_PENDING ? <Spinner /> : <ArrowRightIcon />} colorScheme='blue' isLoading={isSubmitting} type='submit'>
-              Submit
-            </Button>
-          </Center>
-        </form>
+            </Center>
+            {apiState === API_SUCCESS ?
+              <Alert status='success' variant='left-accent' mt={5}>
+                <AlertIcon />
+                RSVP Received. Thank you!
+              </Alert>
+              : null}
+            <Center mt={20}>
+              <Button m={4} rightIcon={apiState === API_PENDING ? <Spinner /> : <ArrowRightIcon />} colorScheme='teal' isLoading={isSubmitting} type='submit'>
+                Submit
+              </Button>
+            </Center>
+          </form>
+        </Box>
       </Center>
     </Container>
   );
