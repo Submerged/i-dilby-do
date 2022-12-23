b067ed0494c76b9b588f8fd24e82b09b2a721de1
Derek Leblanc <derekleblanc@me.com>
2022-12-23 09:25:56 -0330

    Test

--- src/Accommodation/index.tsx
+++ src/Accommodation/index.tsx
@@ -6,7 +6,7 @@ export const Accommodation = () => {
         <Container id="accommodations" maxWidth='100vw' p={0} pt={10}>
             <Center sx={{textAlign: 'center'}}>
                 <Box width='100%' bg='#CCC' pb={[50,50,100]} pt={[20]}>
-                    <Text as='h2'>Accommodations</Text>
+                    <Text as='h2'>Accommodations2</Text>
                     <Text textAlign='center' p={[2,10,10]}>
                         For more information on The Wilds Resort, check out {' '}
                         <Link href= 'https://thewilds.ca' isExternal color='teal.500'>
